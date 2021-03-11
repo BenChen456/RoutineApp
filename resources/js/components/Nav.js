f35273cbee225e91a801e7fd27bf1c00e553b56c
Ben Chen <33535384+BenChen456@users.noreply.github.com>
2021-03-11 16:47:06 -0500

    nav

--- resources/js/components/Nav.js
+++ resources/js/components/Nav.js
@@ -104,7 +104,7 @@ export default function Nav() {
                             </svg>
                             <div style={{fontSize:'22px'}}>Routine</div>
                         </div>
-                        <button onClick={()=>log()}>check</button>
+                        {/* <button onClick={()=>log()}>check</button> */}
                             {mainTaskList.id === undefined ?
                                 null
                                     :
